40928416e9b0fba794f5db8f1f3d05d0a52f8635
Lucas Bastos <lucasbastos@gmail.com>
2020-04-29 16:12:19 -0300

    added cards with real data of covid-19 using material-ui

--- src/App.js
+++ src/App.js
@@ -17,9 +17,6 @@ class App extends React.Component {
     return (
       <div className={styles.container}>
         <Cards data={data} />
-        <Chart />
-        <CountryPicker />
-        <h1>Olá men</h1>
       </div>
     )
   }
